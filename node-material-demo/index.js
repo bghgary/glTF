bc5d9a6b53c46a6cab8f6d3b01a7ca20fdc6ac64
Gary Hsu <bghgary@users.noreply.github.com>
2019-07-25 12:41:51 -0700

    node-material-demo

---
+++ node-material-demo/index.js
@@ -0,0 +1,28 @@
+/// <reference path="./scripts/babylon.d.ts" />
+
+function createScene(canvas, afterRender) {
+    var engine = new BABYLON.Engine(canvas);
+    var scene = new BABYLON.Scene();
+
+    BABYLON.SceneLoader.AppendAsync("./MatCap/BabylonShaderBall_Simple.gltf").then(function () {
+        scene.createDefaultCamera(true, true, true);
+
+        var material = new BABYLON.NodeMaterial("matcap");
+        material.sideOrientation = BABYLON.PBRMaterial.ClockWiseSideOrientation;
+        material.setToDefault();
+        material.build();
+
+        for (var mesh of scene.meshes) {
+            mesh.material = material;
+        }
+
+        engine.runRenderLoop(function() {
+            scene.render();
+            afterRender(engine, scene);
+        });
+
+        scene.debugLayer.show({
+            
+        });
+    });
+}
