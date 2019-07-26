b4c91ac32dd209ecda1f6290aa2aad01900be8f5
Gary Hsu <bghgary@users.noreply.github.com>
2019-07-26 16:10:24 -0700

    node-material-demo updates

--- node-material-demo/index.js
+++ node-material-demo/index.js
@@ -4,20 +4,21 @@ function createScene(canvas, afterRender) {
     var engine = new BABYLON.Engine(canvas);
     var scene = new BABYLON.Scene();
 
-    BABYLON.SceneLoader.AppendAsync("./matcap/BabylonShaderBall_Simple.gltf").then(function () {
+    BABYLON.SceneLoader.ImportMeshAsync(null, "./matcap/BabylonShaderBall.gltf").then(function (result) {
         scene.createDefaultCamera(true, true, true);
         scene.activeCamera.useAutoRotationBehavior = true;
         scene.activeCamera.autoRotationBehavior.idleRotationSpeed = 0.5;
         scene.activeCamera.autoRotationBehavior.idleRotationWaitTime = 0.5;
+
+        var ball = result.meshes[1];
 
         var material = new BABYLON.NodeMaterial("matcap");
         material.sideOrientation = BABYLON.PBRMaterial.ClockWiseSideOrientation;
         material.setToDefault();
         material.build();
 
-        for (var mesh of scene.meshes) {
-            mesh.material = material;
-        }
+        ball.material.dispose();
+        ball.material = material;
 
         engine.runRenderLoop(function() {
             scene.render();
